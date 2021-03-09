feb806bfd7514defda4412bfb554a623d006c979
ShevchukI <ShevchukI>
2021-03-09 10:26:33 +0200

    Initial commit

---
+++ src/main/java/com/shevchuk/machineapp/MachineAppApplication.java
@@ -0,0 +1,13 @@
+package com.shevchuk.machineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachineAppApplication.class, args);
+    }
+
+}
