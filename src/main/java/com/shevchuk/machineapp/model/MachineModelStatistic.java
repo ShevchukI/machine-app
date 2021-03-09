c94508119d244841439532aa7e8400be8be4b1ba
ShevchukI <ShevchukI>
2021-03-09 11:09:32 +0200

    Implemented Machine Model Statistic POJO Data Model. Closes #8

--- src/main/java/com/shevchuk/machineapp/model/MachineModelStatistic.java
+++ src/main/java/com/shevchuk/machineapp/model/MachineModelStatistic.java
@@ -1,4 +1,14 @@
 package com.shevchuk.machineapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MachineModelStatistic {
+
+    private String machineType;
+    private Long number;
 }
